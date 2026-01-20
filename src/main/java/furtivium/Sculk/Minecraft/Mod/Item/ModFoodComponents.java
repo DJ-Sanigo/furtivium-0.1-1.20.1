a05e38ea1e79ec0068f6db17e95d3268c5f3af1e
DJ-Sanigo <benjamin.james.stringer@gmail.com>
2026-01-21 01:18:50 +1100

    furtivium_ingot Made And Added

--- src/main/java/furtivium/Sculk/Minecraft/Mod/Item/ModFoodComponents.java
+++ src/main/java/furtivium/Sculk/Minecraft/Mod/Item/ModFoodComponents.java
@@ -6,5 +6,4 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent VIAL_OF_DORMANT_SCULK = new FoodComponent.Builder().statusEffect(new StatusEffectInstance(StatusEffects.DARKNESS, 200, 0), 1f).build();
-
 }
--- src/main/java/furtivium/Sculk/Minecraft/Mod/Item/ModItems.java
+++ src/main/java/furtivium/Sculk/Minecraft/Mod/Item/ModItems.java
@@ -12,14 +12,14 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static Item VIAL_OF_DORMANT_SCULK = registerItem("vial_of_dormant_sculk",
+    public static final Item VIAL_OF_DORMANT_SCULK = registerItem("vial_of_dormant_sculk",
     new Item(new FabricItemSettings().food(ModFoodComponents.VIAL_OF_DORMANT_SCULK).maxCount(16)));
+    public static final Item FURTIVIUM_INGOT = registerItem("furtivium_ingot", new Item(new FabricItemSettings()));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(VIAL_OF_DORMANT_SCULK);
+        entries.add(FURTIVIUM_INGOT);
     }
-
-
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Furtivium.MOD_ID, name), item);
