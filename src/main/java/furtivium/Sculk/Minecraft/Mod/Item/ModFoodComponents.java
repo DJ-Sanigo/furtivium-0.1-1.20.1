4f035e52b08ce943c339eeb1a82493555029982f
DJ-Sanigo <benjamin.james.stringer@gmail.com>
2026-01-20 13:43:00 +1100

    Broken Item Texture Code, PLEASE HELP!

---
+++ src/main/java/furtivium/Sculk/Minecraft/Mod/Item/ModFoodComponents.java
@@ -0,0 +1,10 @@
+package furtivium.Sculk.Minecraft.Mod.Item;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+    public static final FoodComponent VIAL_OF_DORMANT_SCULK = new FoodComponent.Builder().statusEffect(new StatusEffectInstance(StatusEffects.DARKNESS, 200), 1f).build();
+
+}
--- src/main/java/furtivium/Sculk/Minecraft/Mod/Item/ModItems.java
+++ src/main/java/furtivium/Sculk/Minecraft/Mod/Item/ModItems.java
@@ -1,8 +1,12 @@
 package furtivium.Sculk.Minecraft.Mod.Item;
 
 import furtivium.Sculk.Minecraft.Mod.Furtivium;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -10,10 +14,12 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static Item BOTTLES_OF_DORMANT_SCULK= registerItem("bottle_of_dormant_sculk",
-    new Item(new FabricItemSettings().maxCount(16)));
+    public static Item VIAL_OF_DORMANT_SCULK = registerItem("vial_of_dormant_sculk",
+    new Item(new FabricItemSettings().maxCount(16).food(ModFoodComponents.VIAL_OF_DORMANT_SCULK)));
 
-
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(VIAL_OF_DORMANT_SCULK);
+    }
 
 
 
@@ -23,5 +29,7 @@ public class ModItems {
 
     public static void registerModItems() {
         Furtivium.LOGGER.debug("Registering Mod Items for " + Furtivium.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
 }
