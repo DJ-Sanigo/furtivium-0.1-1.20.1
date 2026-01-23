9ad6e65405e0aebbd848dd5a12de8a508217576a
DJ-Sanigo <benjamin.james.stringer@gmail.com>
2026-01-23 12:17:38 +1100

    Sculk Bones, Music Disc, Crafting, Mineable Blocks, And Block Sounds Added

--- src/main/java/furtivium/Sculk/Minecraft/Mod/FurtiviumDataGenerator.java
+++ src/main/java/furtivium/Sculk/Minecraft/Mod/FurtiviumDataGenerator.java
@@ -3,8 +3,6 @@ package furtivium.Sculk.Minecraft.Mod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import furtivium.Sculk.Minecraft.Mod.datagen.ModItemTagProvider;
-import net.minecraft.client.gui.screen.pack.ResourcePackOrganizer;
-import net.minecraft.data.DataGenerator;
 
 public class FurtiviumDataGenerator implements DataGeneratorEntrypoint {
 	@Override
