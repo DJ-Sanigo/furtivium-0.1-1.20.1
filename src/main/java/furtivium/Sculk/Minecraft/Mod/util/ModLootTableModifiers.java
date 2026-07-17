f0798ef189ae152f88ddb9b8b2998e5865c08de0
DJ-Sanigo <benjamin.james.stringer@gmail.com>
2026-07-17 17:38:40 +1000

    Sears Catalog, SCULK EDITON

--- src/main/java/furtivium/Sculk/Minecraft/Mod/util/ModLootTableModifiers.java
+++ src/main/java/furtivium/Sculk/Minecraft/Mod/util/ModLootTableModifiers.java
@@ -2,12 +2,15 @@ package furtivium.Sculk.Minecraft.Mod.util;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import furtivium.Sculk.Minecraft.Mod.Item.ModItems;
+import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.MatchToolLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.predicate.item.ItemPredicate;
 import net.minecraft.util.Identifier;
 
 public class ModLootTableModifiers {
@@ -16,6 +19,15 @@ public class ModLootTableModifiers {
             new Identifier("minecraft", "chests/ancient_city");
     private static final Identifier WARDEN_ID =
             new Identifier("minecraft", "entities/warden");
+    private static final Identifier SHRIEKER_ID =
+            new Identifier("minecraft", "blocks/sculk_shrieker");
+    private static final Identifier CATALYST_ID =
+            new Identifier("minecraft", "blocks/sculk_catalyst");
+    private static final Identifier SENSOR_ID =
+            new Identifier("minecraft", "blocks/sculk_sensor");
+    private static final Identifier CALIBRATED_ID =
+            new Identifier("minecraft", "blocks/calibrated_sculk_sensor");
+
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, identifier, builder, lootTableSource) ->  {
@@ -124,6 +136,66 @@ public class ModLootTableModifiers {
                         .conditionally(RandomChanceLootCondition.builder(0.75f))
                         .with(ItemEntry.builder(ModItems.SCULK_TENDRIL))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+
+                builder.pool(poolBuilder.build());
+
+            }
+
+            if(SHRIEKER_ID.equals(identifier)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().items(Items.SHEARS)).build())
+                        .with(ItemEntry.builder(ModItems.SCULK_BONE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+
+                builder.pool(poolBuilder.build());
+
+            }
+
+            if(CATALYST_ID.equals(identifier)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().items(Items.SHEARS)).build())
+                        .with(ItemEntry.builder(ModItems.SCULK_BONE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3.0f, 5.0f)).build());
+
+                builder.pool(poolBuilder.build());
+
+            }
+
+            if(SENSOR_ID.equals(identifier)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().items(Items.SHEARS)).build())
+                        .with(ItemEntry.builder(ModItems.SCULK_TENDRIL))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+
+                builder.pool(poolBuilder.build());
+
+            }
+
+            if(CALIBRATED_ID.equals(identifier)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().items(Items.SHEARS)).build())
+                        .with(ItemEntry.builder(ModItems.SCULK_TENDRIL))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+
+                builder.pool(poolBuilder.build());
+
+            }
+
+            if(CALIBRATED_ID.equals(identifier)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.75f))
+                        .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().items(Items.SHEARS)).build())
+                        .with(ItemEntry.builder(Items.AMETHYST_SHARD))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 3.0f)).build());
 
                 builder.pool(poolBuilder.build());
 
