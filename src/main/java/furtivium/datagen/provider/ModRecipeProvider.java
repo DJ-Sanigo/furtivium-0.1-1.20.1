569a2a1d6130bbf117b12cc3bff812cdc71476e5
DJ-Sanigo <benjamin.james.stringer@gmail.com>
2026-09-15 23:56:38 +1000

    HOLY UPDATED AGAIN AGAIN (But This One's For Katsthetics)

--- src/main/java/furtivium/datagen/provider/ModRecipeProvider.java
+++ src/main/java/furtivium/datagen/provider/ModRecipeProvider.java
@@ -191,9 +191,9 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion(hasItem(Items.SCULK), conditionsFromItem(Items.SCULK))
                 .offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, Items.ECHO_SHARD, 2)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, Items.ECHO_SHARD, 3)
                 .pattern("SSS")
-                .pattern("SAS")
+                .pattern("AAA")
                 .pattern("SSS")
                 .input('S', Items.SCULK)
                 .input('A', Items.AMETHYST_SHARD)
